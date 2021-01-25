306fe936779d36f923c0569304f2103904e69d4f
GR <georgerahul24@gmail.com>
2021-01-25 09:42:25 +0530

    Javascript first trial

---
+++ Pages/Biography.js
@@ -0,0 +1,21 @@
+var i=0;
+var content='';
+
+
+function nextMember(value) {
+  i=i+value;
+  document.getElementById("snippetPara").innerHTML = i;
+ if (i==1){
+    content="Hello My name Is george rahul";
+  };
+  if (i==2){
+    content="Hello ";
+  };
+  if (i==3){
+    content=" My ";
+  };
+  if (i==4){
+    content=" name Is george rahul";
+  };
+  document.getElementById("snippetPara").innerHTML = i+content
+}
